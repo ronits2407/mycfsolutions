383c4359394fb4c55f1e4b8269d1467a0ad254d0
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-06-22 22:39:00 +0530

    Add Games on the Train [A] from Codeforces

---
+++ 2236/A - Games on the Train/solution.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pii pair<int, int>
+#define all(v) v.begin(), v.end()
+#define vi vector<int>
+#define vpii vector<pair<int, int>>
+#define ask(v)                              \
+    for (int i = 0; i < (int)v.size(); ++i) \
+        cin >> v[i];
+ 
+#define give(v)                             \
+    for (int i = 0; i < (int)v.size(); ++i) \
+        cout << v[i] << ' ';                \
+    cout << '
+';
+ 
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+void _print() { cerr << "]
+"; }
+template <typename T, typename... Args>
+void _print(T t, Args... args)
+{
+    cerr << t << (sizeof...(args) ? ", " : "");
+    _print(args...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x);
+#else
+#define debug(x...)
+#endif
+ 
+void solve_ronit(int tc)
+{
+    int n;
+    cin >> n;
+ 
+    vi a(n);
+    ask(a);
+    int val = *max_element(all(a));
+    int ans = 0;
+    for (auto &&x : a)
+    {
+        ans = max(ans, val-x);
+    }
+    cout << ans + 1<< endl;
+    
+}
+ 
+int32_t main()
+{
+    cout << fixed << setprecision(20);
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+        solve_ronit(i);
+    return 0;
+}
