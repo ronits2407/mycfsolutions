85ac416466eb6620b5c53fd27f3a1aa46cecd802
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-06-22 21:54:58 +0530

    Add Watermelon [A] from Codeforces

---
+++ 4/A - Watermelon/solution.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main(int argc, char const *argv[])
+{
+    int w;
+    scanf("%d", &w);
+    
+    printf("%s
+", (w % 2 == 0) ? (w == 2) ? "No":"YES":"No");
+    
+    return 0;
+}
