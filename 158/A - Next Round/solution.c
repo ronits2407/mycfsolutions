2fc1ddc4812c1beaadffb659739fdf75a6331e65
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-06-22 21:55:28 +0530

    Add Next Round [A] from Codeforces

---
+++ 158/A - Next Round/solution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int n, k, i;
+    
+    scanf("%d", &n);
+    scanf("%d", &k);
+    
+    int scores[n];
+ 
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        scanf("%d", &scores[i]); 
+    }
+    for (i = 0; (i < n) && (scores[i] != 0) && (scores[i] >= scores[k - 1]); i++);
+    printf("%d", i);
+    
+    return 0;
+}
