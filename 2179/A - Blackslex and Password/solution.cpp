2527904a47af2e3a0bb07abd77c569f5f349ba6f
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-06-22 22:13:25 +0530

    Add Blackslex and Password [A] from Codeforces

---
+++ 2179/A - Blackslex and Password/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+ 
+void solve()
+{
+    int k, x;
+    cin >> k >> x;
+ 
+    cout << k * x + 1 << endl;
+}
+ 
+int32_t main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
