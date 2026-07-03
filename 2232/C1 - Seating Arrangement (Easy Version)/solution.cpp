bcc76589de055952253b75e5016f57e02ac0e1c4
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-07-03 11:17:50 +0530

    Add Seating Arrangement (Easy Version) [C1] from Codeforces

--- 2232/C1 - Seating Arrangement (Easy Version)/solution.cpp
+++ 2232/C1 - Seating Arrangement (Easy Version)/solution.cpp
@@ -179,10 +179,10 @@ void solve_ronit(int tc) {
     
     cout << *max_element(all(occupied)) << endl;
     
+    
 }
  
 int32_t main() {
-    
     boost_ronit;
     cout << fixed << setprecision(20);
  
