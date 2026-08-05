88ebf8210559f52200a924f4df0c1d531a9375b4
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-08-06 00:56:58 +0530

    Add Battle Cows [B] from Codeforces

---
+++ 1951/B - Battle Cows/solution.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+ 
+// PBDS headers
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+// PBDS indexed_set definition
+typedef tree<
+    int, 
+    null_type, 
+    less<int>, 
+    rb_tree_tag, 
+    tree_order_statistics_node_update> 
+    indexed_set;
+ 
+#define int long long
+#define pii pair<int, int>
+#define all(v) v.begin(), v.end()
+#define vi vector<int>
+#define vpii vector<pair<int, int>>
+#define ask(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cin >> v[i];
+ 
+#define give(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cout << v[i] << ' '; \
+    cout << '
+';
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+void _print() { cerr << "]
+"; }
+template <typename T, typename... Args>
+void _print(T t, Args... args)
+{
+    cerr << t << (sizeof...(args) ? ", " : "");
+    _print(args...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...) \
+    cerr << "[" << #x << "] = ["; \
+    _print(x);
+#else
+#define debug(x...)
+#endif
+ 
+void solve_ronit(int tc)
+{
+    int n, k;
+    cin >> n >> k;
+    k--;
+    vi a(n);
+    ask(a, n);
+ 
+    int plusidx = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > a[k])
+        {
+            plusidx = i;
+            break;
+        }
+        
+    }
+    if (plusidx == -1)
+    {
+        cout << n-1 << endl;
+    }
+    else
+    {
+        int secondplusidx = -1;
+        for (int i = plusidx + 1; i < n; i++)
+        {
+            if (a[i] > a[k])
+            {
+                secondplusidx = i;
+                break;
+            }
+            
+        }
+        if (secondplusidx == -1)
+        {
+            secondplusidx = INF;
+        }
+ 
+        int win1 = max(0LL, plusidx - 1);
+        int diff = (plusidx == 0) ? 1 : plusidx;
+        int win2 = min(secondplusidx, k) - diff;
+ 
+        cout << max(win1, win2) << endl;
+    }
+    
+    
+     
+}
+ 
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(20);
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+        solve_ronit(i);
+    return 0;
+}
