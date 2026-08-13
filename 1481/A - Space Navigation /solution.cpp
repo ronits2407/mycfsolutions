bd4e0ddbb103b4eca44e7b86f98098d0d63f9520
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-08-13 14:03:04 +0530

    Add Space Navigation  [A] from Codeforces

---
+++ 1481/A - Space Navigation /solution.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+ 
+// PBDS headers
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+// PBDS indexed_set definition
+typedef tree<
+    int, 
+    null_type, 
+    less<int>, 
+    rb_tree_tag, 
+    tree_order_statistics_node_update> 
+    indexed_set;
+ 
+#define int long long
+#define pii pair<int, int>
+#define all(v) v.begin(), v.end()
+#define vi vector<int>
+#define vpii vector<pair<int, int>>
+#define ask(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cin >> v[i];
+ 
+#define give(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cout << v[i] << ' '; \
+    cout << '
+';
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+void _print() { cerr << "]
+"; }
+template <typename T, typename... Args>
+void _print(T t, Args... args)
+{
+    cerr << t << (sizeof...(args) ? ", " : "");
+    _print(args...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...) \
+    cerr << "[" << #x << "] = ["; \
+    _print(x);
+#else
+#define debug(x...)
+#endif
+ 
+int power(int base, int exp) {
+    int res = 1;
+    while (exp) {
+        if (exp % 2) res = (res * base) % MOD;
+        base = (base * base) % MOD;
+        exp /= 2;
+    }
+    return res;
+}
+ 
+int nCr(int n, int r) {
+    if (r < 0 || r > n) return 0;
+    if (r > n - r) r = n - r;
+    
+    int num = 1, den = 1;
+    for (int i = 0; i < r; i++) {
+        num = (num * (n - i)) % MOD;
+        den = (den * (i + 1)) % MOD;
+    }
+    
+    return (num * power(den, MOD - 2)) % MOD;
+}
+ 
+void solve_ronit(int tc)
+{
+    int x, y;
+    cin >> x >> y;
+    string s;
+    cin >> s;
+    int U = count(all(s), 'U');
+    int R = count(all(s), 'R');
+    int D = count(all(s), 'D');
+    int L = count(all(s), 'L');
+ 
+    if ((x > 0 && R < x) || (x < 0 && L < abs(x)))
+    {
+        cout << "NO" << "
+";
+        return;
+    }
+    if ((y > 0 && U < y) || (y < 0 && D < abs(y)))
+    {
+        cout << "NO" << "
+";
+        return;
+    }
+    cout << "YES" << "
+";
+    
+}
+ 
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(20);
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+        solve_ronit(i);
+    return 0;
+}
